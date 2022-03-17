88223c383347961c5cd69dd416f1f5aa0c5259d7
AjabiOladipupo <dipoajabisam95@gmail.com>
2022-03-17 12:38:45 -0700

    Task Five

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 when everthing works fine
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; ++n)
+	{
+		printf(%d" , n);
+	}
+	putchar(10);
+	return (0);
+}
