c8cacc317bb25fde7133a1a6373e31c34bacb1b9
AjabiOladipupo <dipoajabisam95@gmail.com>
2022-03-17 15:40:53 -0700

    Main.h

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Nested loops readme file
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_tables();
+void print_to_98(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int -abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
