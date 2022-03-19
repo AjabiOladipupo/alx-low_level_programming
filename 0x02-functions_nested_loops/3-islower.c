0a7d902afde3ec98ed70c043f5e1aa619347df75
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-03-19 10:26:02 +0100

    Task Three

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
+© 2022 GitHub, Inc.
+Terms
+P
