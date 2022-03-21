fea4e19043455a3bea4b00ecfdd5537bfe2e5630
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-03-21 01:42:04 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@
  *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
-void print times_table(int n)
+void print_times_table(int n)
 {
 	int num, mult, prod;
 
