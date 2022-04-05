c4f15d5d9eb6749ecccf170563289d66a3a36c23
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-04-06 00:32:55 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,19 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-/**
-* void prototypes()
-* int prototypes()
-*/
-
-char *_memset(char *, char, unsigned int);
-char *_memcpy(char *, char *, unsigned int);
-char *_strchr(char *, char);
-unsigned int _strspn(char *, char *);
-char *_strpbrk(char *, char *);
-char *_strstr(char *, char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int *, int);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /*MAIN_H*/
+#endif
