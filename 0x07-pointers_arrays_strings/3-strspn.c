0d8a2682e0824615942ae7e6705ed94841d313c8
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-04-05 18:12:05 +0100

    Task Three

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,46 +1,24 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: string to be searched
- * @accept: string to be used
- *
- * Return: number of bytes in the initial segment of s which are part of accept
+ * @s: input string to search for substring
+ * @accept: characters that prefix substring must include
+ * Return: length of prefix substring
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	int match = 0;
+	unsigned int i, j, a_len = 0, len = 0;
 
-	while (s[i] != '\0')
-	{
-		if (_strchr(accept, s[i]) == NULL)
-			break;
-		match++;
-		i++;
-	}
-
-	return (match);
-}
-
-/**
- * _strchr - locates a char in a string
- * @s: string to be searched
- * @c: char to be checked
- *
- * Return: pointer to the first occurence of c in s
- */
-
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	for (; s[i] != c && s[i] != '\0'; i++);
-
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+	while (accept[a_len] != '\0')
+		a_len++;
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < a_len; j++)
+			if (s[i] == accept[j])
+				len++, j = a_len;
+			else
+				if (j == a_len - 1)
+					goto exit;
+exit: return (len);
 }
