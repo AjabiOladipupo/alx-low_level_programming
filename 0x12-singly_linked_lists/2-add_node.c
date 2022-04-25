02a1d72b93dfb0ef80330d00f7e96da1fb7d3103
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-04-25 14:50:06 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,45 +1,42 @@
-/*
- * File: 2-add_node.c
- * Auth: Oladipupo Ajabi
- */
-
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning
- *            of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
+ * add_node - adds a node to the beginning of a linked list
+ * @head: pointer to the head of the list
+ * @str: string to be used as content of the node
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ * Return: address of the newly added node
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	list_t *new_node;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (dup == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node != NULL)
 	{
-		free(new);
-		return (NULL);
+		new_node->str = strdup(str);
+		new_node->len = _strlen(str);
+		new_node->next = *head;
 	}
+	else
+		return (NULL);
+	if (*head != NULL)
+		new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
 
-	for (len = 0; str[len];)
-		len++;
+/**
+ * _strlen - counts the length of a string
+ * @str: string to be counted
+ *
+ * Return: the length of the string
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
 
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
