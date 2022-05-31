fb5da48bfd5bf09431af850c9ff56de4a05e69a0
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-05-31 19:30:54 +0100

    mainh

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,7 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
