819beeec53a2bf41f32ca2f07b253b0cbec8d7b4
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-05-31 17:40:28 +0100

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - function to check if number is absolute
+ * Return: int
+ * @n: int
+ *
+ * Description:
+ * Author: Oladipupo Ajabi
+ * Date: May 31, 2022
+ */
+
+int _abs(int n)
+{
+
+int r;
+
+r = abs(n);
+return (r);
+
+}
