53ce0864aa0d7741f94bd65f349fd22e2545f669
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-05-31 17:27:19 +0100

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function to compare 2 strings
+ * Return: int
+ * @s1: char ptr
+ * @s2: char ptr
+ *
+ * Description:
+ * Author: Oladipupo Ajabi
+ * Date: May 31, 2022
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
