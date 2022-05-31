fd87c666f51e0cde99e11a9f3ca6d30ca03f9fa1
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-05-31 16:58:33 +0100

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - function to return the lenght of the str
+ * Return: void
+ * @s: char pointer
+ *
+ * Description:
+ * Author: Oladipupo Ajabi
+ * Date: May 31, 2022
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
