09209612a423b7b6c4c135755188e630180cd8fe
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-05-31 17:26:05 +0100

    Create 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function to print a str
+ * Return: void
+ * @str: str pointer
+ *
+ * Description:
+ * Author: Oladipupo Ajabi
+ * Date: May 31, 2022
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
