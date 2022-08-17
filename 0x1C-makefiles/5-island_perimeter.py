c259ffa865617807131141ed2002e57e2af2dd10
AjabiOladipupo <dipoajabisam95@gmail.com>
2022-08-17 11:56:33 +0100

    make file task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0 or i == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0 or j == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
